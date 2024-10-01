b7dbc39a58308bd6bd7ce28fa6b3e2788fdae3dd
Rahul Somasunderam <rahulsom@noreply.github.com>
2024-10-01 19:29:02 +0530

    chore: Migrate root project to build.gradle.kts
    That makes it consistent with all other gradle scripts.

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+import nebula.plugin.contacts.Contact
+import nebula.plugin.contacts.ContactsExtension
+
+buildscript {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+plugins {
+    alias(libs.plugins.waenaRoot)
+    alias(libs.plugins.waenaPublished).apply(false)
+    alias(libs.plugins.dgs).apply(false)
+    alias(libs.plugins.download).apply(false)
+}
+
+allprojects {
+    group = "io.github.pulpogato"
+
+    extensions.findByType<ContactsExtension>()?.apply {
+        addPerson("rahulsom@noreply.github.com", delegateClosureOf<Contact> {
+            moniker("Rahul Somasunderam")
+            roles("owner")
+            github("https://github.com/rahulsom")
+        })
+    }
+}
+
+subprojects {
+    repositories {
+        mavenCentral()
+    }
+}
